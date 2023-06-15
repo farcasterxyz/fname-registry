b1aaeabd54e71fd770cb55f9b3cc222652d3c4dd
Shane da Silva <shane@dasilva.io>
2023-06-15 16:58:41 -0700

    Add healthcheck

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import { getDb } from './db.js';
 
 const PAGE_SIZE = 100;
 
 const db = await getDb();
 export const app: Express = express();
-app.get('/transfers', async (req: Request, res: Response) => {
-    const since = req.query.since ?? 0;
-    db.from('transfers')
-        .select('id', 'timestamp', 'username', 'owner', 'from', 'to', 'signature')
-        .where('id', '>', since)
-        .limit(PAGE_SIZE)
-        .then((rows: any) => {
-            res.send({transfers: rows});
-        });
+
+app.get('/transfers', async (req, res) => {
+  const since = req.query.since ?? 0;
+  db.from('transfers')
+    .select('id', 'timestamp', 'username', 'owner', 'from', 'to', 'signature')
+    .where('id', '>', since)
+    .limit(PAGE_SIZE)
+    .then((rows: any) => {
+      res.send({transfers: rows});
+    });
+});
+
+app.get('/_health', async (_req, res) => {
+  res.send({ status: 'ok' });
 });

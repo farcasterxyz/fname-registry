7a36bc35be071bbf85634b2006855c9c0bc1c307
Shane da Silva <shane@dasilva.io>
2024-10-22 00:13:12 -0700

    Increase size of instance (#406)

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import './tracing.js';
 import { app } from './app.js';
 import { log } from './log.js';
 
-const port = process.env.PORT || '3000';
-const server = app.listen(port, () => {
-  log.info(`⚡️[server]: Server is running at http://localhost:${port}`);
+const port = parseInt(process.env.PORT || '3000');
+const server = app.listen(port, '0.0.0.0', () => {
+  log.info(`⚡️[server]: Server is running at http://0.0.0.0:${port}`);
 });
 
 // Ensure this is larger than the ALB's keep-alive timeout (default 60s).

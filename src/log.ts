a3fdb50dbb73c2a7254de9152e0d72d70a799a5f
Shane da Silva <shane@dasilva.io>
2025-08-13 09:38:30 -0700

    Improve logging integration

--- src/log.ts
+++ src/log.ts
@@ -1,12 +1,20 @@
+import { ENVIRONMENT } from './env.js';
 import pino from 'pino';
 
 export const log = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
+  formatters: {
+    level: (label) => ({ level: label }),
   },
+  ...(ENVIRONMENT === 'dev'
+    ? {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+          },
+        },
+      }
+    : {}),
 });
 
 export type Logger = pino.Logger;
